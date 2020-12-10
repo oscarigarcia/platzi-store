3823723937cb736498034926404037d0f77f48b9
oscary garcia <oscarygarcia@oscarys-MacBook-Pro.local>
2020-12-10 13:06:41 -0400

    Add page table

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,17 +3,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { AdminRoutingModule } from "./admin-routing.module";
-import { ProductFormComponent } from "./components/product-form/product-form.component";
-import { TableComponent } from "./components/table/table.component";
-import { ReactiveFormsModule } from "@angular/forms";
 import { NavComponent } from "./components/nav/nav.component";
-import { LayoutModule } from "@angular/cdk/layout";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatButtonModule } from "@angular/material/button";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatIconModule } from "@angular/material/icon";
-import { MatListModule } from "@angular/material/list";
+import { ProductFormComponent } from "./components/product-form/product-form.component";
 import { ProductsListComponent } from "./components/products-list/products-list.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { TableComponent } from "./components/table/table.component";
 
 @NgModule({
   declarations: [
@@ -27,12 +21,6 @@ import { ProductsListComponent } from "./components/products-list/products-list.
     AdminRoutingModule,
     ReactiveFormsModule,
     MaterialModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
   ],
 })
 export class AdminModule {}
