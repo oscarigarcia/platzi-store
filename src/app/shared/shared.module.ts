d8fac8722bf0233aad351d367e81162ab1d8bc32
oscary garcia <oscarygarcia@oscarys-MacBook-Pro.local>
2020-12-09 23:26:18 -0400

    Add styles

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,28 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 
-import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
-import { HighlightDirective } from './directives/highlight/highlight.directive';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { ExponentialPipe } from "./pipes/exponential/exponential.pipe";
+import { HighlightDirective } from "./directives/highlight/highlight.directive";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { MaterialModule } from "./../material/material.module";
+import { MatBadgeModule } from "@angular/material/badge";
 
 @NgModule({
   declarations: [
     ExponentialPipe,
     HighlightDirective,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   exports: [
     ExponentialPipe,
     HighlightDirective,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MatBadgeModule,
   ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ]
+  imports: [CommonModule, RouterModule, MaterialModule, MatBadgeModule],
 })
-export class SharedModule { }
+export class SharedModule {}
