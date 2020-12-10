570d7d5671b2fb9f96537e47bb46bca6216a0971
oscary garcia <oscarygarcia@oscarys-MacBook-Pro.local>
2020-12-10 12:25:01 -0400

    table

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,9 @@
+import { ProductsListComponent } from "./components/products-list/products-list.component";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ProductFormComponent } from "./components/product-form/product-form.component";
 import { NavComponent } from "./components/nav/nav.component";
+import { TableComponent } from "./components/table/table.component";
 
 const routes: Routes = [
   {
@@ -11,6 +13,14 @@ const routes: Routes = [
       {
         path: "create",
         component: ProductFormComponent,
+      },
+      {
+        path: "table",
+        component: TableComponent,
+      },
+      {
+        path: "products",
+        component: ProductsListComponent,
       },
     ],
   },
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,17 +4,24 @@ import { CommonModule } from "@angular/common";
 
 import { AdminRoutingModule } from "./admin-routing.module";
 import { ProductFormComponent } from "./components/product-form/product-form.component";
+import { TableComponent } from "./components/table/table.component";
 import { ReactiveFormsModule } from "@angular/forms";
-import { NavComponent } from './components/nav/nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { NavComponent } from "./components/nav/nav.component";
+import { LayoutModule } from "@angular/cdk/layout";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { ProductsListComponent } from "./components/products-list/products-list.component";
 
 @NgModule({
-  declarations: [ProductFormComponent, NavComponent],
+  declarations: [
+    ProductFormComponent,
+    NavComponent,
+    ProductsListComponent,
+    TableComponent,
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule,
